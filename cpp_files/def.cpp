4495f3ec126377d13e3888c170b100e0ef9a37e4
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-05-31 12:25:44 +0530

    Auto backup: 2026-05-31 12:25:44

--- cpp_files/def.cpp
+++ cpp_files/def.cpp
@@ -1,15 +1,21 @@
 #include <cstdio>
 
 struct Highlander {
-    private:
-    int x;
-    public: 
+    int *x = new int;
     Highlander(int new_int) {
-        x = new_int;
-        printf("The value of x is %d and the address of x is %p\n", x, &x);
+        *x = new_int;
+        printf("The value of x is %d and the address of x is %p\n", x, x);
+    }
+    Highlander(const Highlander& other) = delete;
+    Highlander& operator=(const Highlander& other) = delete;
+    ~Highlander() {
+        printf("The struct variable is destroyed....\n");
     }
 };
 
 int main(void) {
-
+    Highlander a{23};
+    Highlander b{a};
+    printf("The value of x in b is %d and the address of x in b is %p\n", *b.x, b.x);
+    return 0;
 }
