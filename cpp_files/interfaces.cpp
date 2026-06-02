539a37336bf5ba22c4a69139c91cc86e849337e4
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-06-02 17:46:28 +0530

    Auto backup: 2026-06-02 17:46:28

---
+++ cpp_files/interfaces.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+#include <stdexcept>
+
+int main(void) {
+    
+}
--- cpp_files/poly_intro.cpp
+++ cpp_files/poly_intro.cpp
@@ -1,21 +1,56 @@
 #include <cstdio>
 #include <stdexcept>
+
+enum class LoggerType {
+    Console,
+    File
+};
+
 struct ConsoleLogger {
     void log_transfer(long from, long to, double amount) {
-        printf("%ld -> %ld : %f\n", from, to, amount);
+        printf("[console] %ld -> %ld : %lf\n", from, to, amount);
+    }
+};
+
+struct FileLogger {
+    void log_transfer(long from, long to, double amount) {
+        printf("[file] %ld, %ld, %lf\n", from, to, amount);
     }
 };
 
 struct Bank {
-    ConsoleLogger logger;
-    void transfer(long from, long to, double amount) {
-        logger.log_transfer(from, to, amount);
+    Bank() : type(LoggerType::Console) {}
+
+    void set_logger(LoggerType logger_type) {
+        type = logger_type;
     }
+
+    void make_transfer(long from, long to, double amount) {
+        switch(type) {
+            case LoggerType::Console:
+                console_logger.log_transfer(from, to, amount);
+                break;
+            case LoggerType::File:
+                file_logger.log_transfer(from, to, amount);
+                break;
+            default:
+                throw std::runtime_error("Invalid logger type\n");
+        }
+    }
+
+    private:
+        LoggerType type;
+        ConsoleLogger console_logger;
+        FileLogger file_logger;
 };
+
 
 int main(void) {
     Bank bank;
-    bank.transfer(123, 456, 100.0);
-    bank.transfer(456, 123, 50.0);
+    bank.set_logger(LoggerType::Console);
+    bank.make_transfer(45234, 2454, 4125.13);
+    bank.set_logger(LoggerType::File);
+    bank.make_transfer(8586, 22354, 876.25);
+    printf("Program finished successfully\n");
     return 0;
 }
