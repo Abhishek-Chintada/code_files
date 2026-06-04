c2691c2b94be4edc3781fefda6cc5654c2492216
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-06-04 20:20:24 +0530

    Auto backup: 2026-06-04 20:20:24

--- cpp_files/test.cpp
+++ cpp_files/test.cpp
@@ -1,12 +1,9 @@
 #include <cstdio>
-#include <cstring>
 
 int main(void) {
-    const char *a = new char[10];
-    a = "lavada";
-    printf("%s\n", a);
-    char *b = new char[10];
-    std::strncpy(b, a, size_t(10));
-    printf("%s\n", b);
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    printf("%d\n",sum);
     return 0;
 }
