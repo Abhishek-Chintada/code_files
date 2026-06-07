c9e8007efea3728bcbed15938fec608f926c0c7c
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-06-07 11:55:42 +0530

    Auto backup: 2026-06-07 11:55:42

---
+++ cpp_files/compoly.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+template<typename T1, typename T2>
+char printing(T1 a, T2 b) {
+   return b+2;
+}
+
+int main(void) {
+    char ch =  printing(23, 'a');
+    printf("%c\n", ch);
+    return 0;
+}
