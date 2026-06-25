23b75f7fb1317afe30df984c13e5cc073667f9f0
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-06-25 20:15:15 +0530

    Auto backup: 2026-06-25 20:15:15

--- cpp_files/directives.cpp
+++ cpp_files/directives.cpp
@@ -1,7 +1,35 @@
+// Getting trashed by frickin' shit of directives and namespaces.
 #include <cstdio>
 
+namespace Labbe {
+    enum class Color { // VIBGYOR ra luccha.
+        red,
+        blue,
+        green,
+        violet,
+        yellow,
+        indigo,
+        orange
+    };
 
+    struct Lavdesh {  // POD
+        const char* name;
+        Color color;
+    };
+
+    bool check_is_red(const Lavdesh& lavdesh) {
+        return lavdesh.color == Labbe::Color::red;
+    }
+}
+
+using namespace Labbe;
 
 int main(void) {
+    Color color = Color::red;
+    Lavdesh lavdesh{"BSDK", color};
+    auto boolralabbe = check_is_red(lavdesh);
+    if(boolralabbe) {
+        printf("True hai yeh sab bool.\n");
+    }
     return 0;
 }
