d79968eec04fcab81c4b3031a3f1c97c0c6a6d94
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-06-26 11:03:44 +0530

    Auto backup: 2026-06-26 11:03:44

---
+++ cpp_files/strivers/io.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+
+int main(void) {
+    std::cout << "Na Lavada\n";
+    int x {};
+    std::cout << "Enter a number\n";
+    std::cin >> x;
+    std::cout << "This is the answer here " << 2*x << "\n";
+    return 0;
+}
