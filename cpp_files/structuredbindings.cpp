5877bbc0d47b066825ff8f35651c52f5d7a34201
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-06-28 14:42:10 +0530

    Auto backup: 2026-06-28 14:42:10

--- cpp_files/structuredbindings.cpp
+++ cpp_files/structuredbindings.cpp
@@ -1,4 +1,26 @@
 #include <cstdio>
 #include <stdexcept>
 
-struct te
+struct textFile {
+    bool status;
+    const char* contents;
+    size_t bytes;
+};
+textFile read_text(const char* text) {
+    const char contents[] {"Sometimes the goat is you."};
+    return textFile{
+            true, 
+            contents,
+            sizeof(contents)
+        };
+}
+
+int main(void) {
+    const auto [success, contents, size] = read_text("Labbe.txt");
+    if(success) {
+        printf("These are the contents :\n%s\n", contents);
+    } else {
+        printf("<error parsing the file>\n");
+    }
+    return 0;
+}
