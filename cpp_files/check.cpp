e1d6f0727ffa37e59f8b8e03c0b45cadcc2ebf98
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-07-09 18:32:54 +0530

    Mac backup: 2026-07-09 18:32:54

--- cpp_files/check.cpp
+++ cpp_files/check.cpp
@@ -1,11 +1,5 @@
 #include <cstdio>
 
-int main() {
-    int a = 10;
-    int n = 100;
-    for(int i = 0; i < n; i++) {
-        a = a + 1;
-        printf("%d , ", a);
-    }
-    printf("\n");
+int sum(int a, int b) {
+    return a + b ;
 }
---
+++ cpp_files/check_0.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include "check.cpp"
+
+int main(void) {
+    int res = sum(10, 12);
+    printf("%d\n", res);
+    printf("File is included successfully.\n");
+    return 0;
+}
