62bf2574c96f350dd8a5d2b9811cac69329c87fc
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-08-02 11:39:32 +0530

    Arch backup: 2026-08-02 11:39:32

--- cpp_files/car_skeleton.cpp
+++ cpp_files/car_skeleton.cpp
@@ -1,41 +1,22 @@
 // services
 struct speedUpdate {
-  double velocity;
+  double velocity_mps;
 };
 struct carDetected {
-  double distance;
-  double velocity;
+  double distance_m;
+  double velocity_mps;
 };
 struct brakeCommand {
-  double timeToCollision;
+  double timeToCollision_s;
 };
 
-// service bus
-struct serviceBus {
-  void publish(const brakeCommand &);
-  // --snip--
-};
-
-// Auto Brake struct
-template <typename T> 
-struct autoBrake {
-  autoBrake(const T &publish);
-  void observe(const speedUpdate &);
-  void observe(const carDetected &);
-  // --snip--
-private:
-  const T &publish;
-};
-
-int main(void) {
-  serviceBus bus;
-  autoBrake auto_brake { [&bus] (const auto* cmd){
-                    bus.publish(cmd);
-              }
-    };
-  while(true) {
-    auto_brake.observe(speedUpdate{10L});
-    auto_brake.observe(carDetected{250L, 25L});
-  }
-  return 0;
+template <typename T>
+struct AutoBrake {
+  AutoBrake(const T& publish) : publish{publish} {}
+  void observe(const speedUpdate& cd) {}
+  void observe(const car)
+  private:
+  double collision_threshold_s;
+  double speed_mps;
+  const T& publish;
 }
