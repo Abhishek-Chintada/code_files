f4850f526d613a0faa24b652ae259679adbf0fb9
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-08-23 00:21:27 +0530

    Arch backup: 2026-08-23 00:21:27

--- cfiles/DSA/stack_linked.c
+++ cfiles/DSA/stack_linked.c
@@ -2,6 +2,23 @@
 #include <time.h>
 #include <stdlib.h>
 
+typedef struct {
+    int element;
+    void* next;
+}Node;
+
+Node *head = NULL;
+
+void addNode(int x) {
+    if(head == NULL) {
+        head = (Node*)malloc(sizeof(Node));
+    } else {
+        head->next = (Node*)malloc(sizeof(Node));
+        
+    }
+}
+
 int main(void) {
+
     return 0;
 }
