e5214e0ae1e60733fb18578a1dd13ce1ccfe96ef
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-08-24 11:12:04 +0530

    Mac backup: 2026-08-24 11:12:04

--- cfiles/himantth/intro.c
+++ cfiles/himantth/intro.c
@@ -3,8 +3,8 @@ int main()
 {
   int a,b, min;
     printf("int two integers:");
-    scanf("%d %d",&a,&b);
-    min=(a+b-)/2
-    printf("minimum=%d\n",min);
+    scanf("%d %d",&a,&b;
+    min(a+b-)/2
+    printf("minimum=%d\n min);
     return 0;
 }
