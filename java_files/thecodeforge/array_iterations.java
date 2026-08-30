f60edf269acbf80c467ff2ead0839cf4be18da0f
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-08-31 00:01:20 +0530

    Arch backup: 2026-08-31 00:01:20

--- java_files/thecodeforge/array_iterations.java
+++ java_files/thecodeforge/array_iterations.java
@@ -12,13 +12,24 @@ public class array_iterations {
             System.out.println("Month : " + monthNames[i] + " -> " + monthlyRainfall[i] + "mm");
         }
 
-        // Pattern - II i.e the for-each loop
+        // Pattern - II i.e the for-each loop; Note -> Only READ no WRITE
         int totalRainfall = 0;
         for(int rainfall : monthlyRainfall) {
             totalRainfall += rainfall;
         }
         System.out.println("Total Average Annual Rainfall : " + totalRainfall);
         System.out.printf("Monthly Average : %.1f \n", (double)totalRainfall/monthlyRainfall.length);
+
+        // Finding the maximum of all (The Running Chamion Pattern)
+        int peakRainfall = monthlyRainfall[0];
+        int peakMonth = 0;
+        for(int i = 0; i < monthlyRainfall.length; i++) {
+            if(monthlyRainfall[i] > peakRainfall) {
+                peakRainfall = monthlyRainfall[i];
+                peakMonth = i;
+            }
+        }
+        System.out.println("Peak Rainfall : " + peakRainfall + "mm in the month of " + monthNames[peakMonth] + ".");
 
         System.out.println("<console> EOP");
     }
