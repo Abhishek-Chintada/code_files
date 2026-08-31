f298390ed8af3630de5dbf2c9fa64cd100047f66
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-08-31 14:43:12 +0530

    Mac backup: 2026-08-31 14:43:12

--- cfiles/DSA/stack_linked.c
+++ cfiles/DSA/stack_linked.c
@@ -8,17 +8,64 @@ typedef struct {
 }Node;
 
 Node *head = NULL;
+Node *tail = NULL;
+size_t current_top = 0;
 
-void addNode(int x) {
-    if(head == NULL) {
-        head = (Node*)malloc(sizeof(Node));
+void createList(int x) {
+    if(head != NULL) {
+        printf("The Linked List is already created.\n");
+        return;
     } else {
-        head->next = (Node*)malloc(sizeof(Node));
-        
+        head = (Node*)malloc(sizeof(Node));
+        head->element = x;
+        head->next = NULL;
+        tail = head;
+        current_top = 0;
+        return;
     }
 }
 
-int main(void) {
+void addNode(int x) {
+    if(head == NULL) {
+        printf("The linked list is not yet init.\n");
+        return;
+    }
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    tmp->element = x;
+    tmp->next = NULL;
+    tail->next = tmp;
+    tail = tmp;
+    current_top++;
+    return;
+}
 
+void remNode() {
+    Node *labbe = head;
+    for(size_t i = 0; i < current_top-1; i++) {
+        labbe = labbe->next;
+    }
+    labbe->next = NULL;
+    current_top = current_top - 1;
+}
+
+void printList() {
+    Node* tmp = head;
+    printf("[ ");
+    for(size_t i = 0; i < current_top; i++) {
+        printf("%d ", tmp->element);
+        tmp = tmp->next;
+    }
+    printf("%d ]\n", tmp->element);
+}
+
+int main(void) {
+    createList(23);
+    addNode(34);
+    addNode(54);
+    printList();
+    printf("<console> Number of elements in list : %zu\n", current_top+1);
+    remNode();
+    printList();
+   printf("<console> Number of elements in list : %zu\n", current_top+1); 
     return 0;
 }
