911d450814a450013847ad3a7d5cdde81e4c0091
Abhishek_Chintada <abhishekchintada001@gmail.com>
2026-09-03 14:01:14 -0400

    Auto-commit: 2026-09-03 14:01:14

--- cpp_files/cmd_arguments.cpp
+++ cpp_files/cmd_arguments.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 #include <cstdint>
-
+// sanka naaki pondi bro!
 int main(int argc, char** argv) {
     printf("No. of arguments : %d\n", argc);
     for(size_t i{}; i < argc; i++) {
