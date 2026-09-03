021d54d1154bac2435b141551a898e825a0f2309
Abhishek_Chintada <abhishekchintada001@gmail.com>
2026-09-03 13:53:42 -0400

    Auto-commit:

---
+++ cpp_files/test_debian.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void) {
+	int a {};
+	std::cout << "Enter a number : " << std::endl;
+	std::cin >> a;
+	std::cout << "The double of the entered number is : " << a*2 << std::endl;
+	return 0;	
+}
