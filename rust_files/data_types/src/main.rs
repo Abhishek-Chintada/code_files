97c5a63120d6d31a00a3900c429642d4f48954f7
Abhishek-Chintada <abhishekchintada001@gmail.com>
2026-09-17 23:29:25 +0530

    Auto-commit from Arch setup: 2026-09-17 23:29:25

---
+++ rust_files/data_types/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x: i8 = 10;
+    println!("{}", x);
+
+    let y: u8 = 10;
+    println!("{}", y);
+}
---
+++ rust_files/data_types/target/debug/deps/data_types-452b441e00f5d727.d
@@ -0,0 +1,5 @@
+/home/abhishekchintada/slanderishCode/code_files/rust_files/data_types/target/debug/deps/data_types-452b441e00f5d727.d: src/main.rs
+
+/home/abhishekchintada/slanderishCode/code_files/rust_files/data_types/target/debug/deps/data_types-452b441e00f5d727: src/main.rs
+
+src/main.rs:
